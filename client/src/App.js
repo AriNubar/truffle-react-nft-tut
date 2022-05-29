8bc1c449a37ccf630f872ce35581cec15d2e80fd
AriNubar <nubarboyaci@hotmail.com>
2022-05-29 06:36:16 +0200

    small error handling parts added

--- client/src/App.js
+++ client/src/App.js
@@ -12,30 +12,38 @@ const App = () => {
   const [coders, setCoders] = useState([]);
 
   const mint = () => {
-    contract.methods.mint(mintText).send({ from: account }, (error)=>{
-      console.log("it worked")
-      if(!error){
-        setCoders([...coders, mintText])
-        setMintText("");
-      }
-    });
+    if (mintText.length > 0) {
+      console.log("minting");
+      contract.methods.mint(mintText).send({ from: account }, (error) => {
+        console.log("it worked")
+        if (!error) {
+          setCoders([...coders, mintText])
+          setMintText("");
+        }
+        else {
+          console.log(error);
+        }
+      });
+    }
+    else{
+      console.log("no text");
+    }
   }
 
   const loadNFTS = async (contract) => {
     const totalSupply = await contract.methods.totalSupply().call();
     console.log(totalSupply);
-    console.log("trying again");
     let results = [];
-    for(let i = 0; i < totalSupply; i++){
+    for (let i = 0; i < totalSupply; i++) {
       let coder = await contract.methods.coders(i).call();
       results.push(coder)
     }
     setCoders(results);
   }
 
-  const loadWeb3Account = async (web3) =>{
+  const loadWeb3Account = async (web3) => {
     const accounts = await web3.eth.getAccounts();
-    if(accounts){
+    if (accounts) {
       setAccount(accounts[0]);
     }
   }
@@ -43,7 +51,7 @@ const App = () => {
   const loadWeb3Contract = async (web3) => {
     const networkId = await web3.eth.net.getId();
     const networkData = CryptoCoder.networks[networkId];
-    if(networkData){
+    if (networkData) {
       const abi = CryptoCoder.abi;
       const address = networkData.address;
       const contract = new web3.eth.Contract(abi, address);
@@ -52,7 +60,7 @@ const App = () => {
     }
   }
 
-  useEffect(async ()=>{
+  useEffect(async () => {
     const web3 = await getWeb3();
     await loadWeb3Account(web3);
     const contract = await loadWeb3Contract(web3);
@@ -60,38 +68,38 @@ const App = () => {
   }, [])
 
   return (
-  <div>
-    <nav className="navbar navbar-light bg-light px-4">
-      <a className="navbar-brand" href="#">Crypto Coders</a>
-      <span>{account}</span>
-    </nav>
-    <div className="container-fluid mt-5">
-      <div className="row">
-        <div className="col d-flex flex-column align-items-center">
-          <img className="mb-4" src="https://avatars.dicebear.com/api/pixel-art/naz.svg" alt="" width="72"/>
-          <h1 className="display-5 fw-bold">Crypto Coders</h1>
-          <div className="col-6 text-center mb-3" >
-            <p className="lead text-center">These are some of the most highly motivated coders in the world! We are here to learn coding and apply it to the betterment of humanity. We are inventors, innovators, and creators</p>
-            <div>
-              <input 
-                type="text"
-                value={mintText}
-                onChange={(e)=>setMintText(e.target.value)}
-                className="form-control mb-2"
-                placeholder="e.g. Naz" />
-              <button onClick={mint} className="btn btn-primary">Mint</button>
+    <div>
+      <nav className="navbar navbar-light bg-light px-4">
+        <a className="navbar-brand" href="#">Crypto Coders</a>
+        <span>{account}</span>
+      </nav>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <div className="col d-flex flex-column align-items-center">
+            <img className="mb-4" src="https://avatars.dicebear.com/api/pixel-art/naz.svg" alt="" width="72" />
+            <h1 className="display-5 fw-bold">Crypto Coders</h1>
+            <div className="col-6 text-center mb-3" >
+              <p className="lead text-center">These are some of the most highly motivated coders in the world! We are here to learn coding and apply it to the betterment of humanity. We are inventors, innovators, and creators</p>
+              <div>
+                <input
+                  type="text"
+                  value={mintText}
+                  onChange={(e) => setMintText(e.target.value)}
+                  className="form-control mb-2"
+                  placeholder="e.g. Ari" />
+                <button onClick={mint} className="btn btn-primary">Mint</button>
+              </div>
             </div>
-          </div>
-          <div className="col-8 d-flex justify-content-center flex-wrap">
-            {coders.map((coder, key)=><div className="d-flex flex-column align-items-center" key={key}>
-                  <img width="150" src={`https://avatars.dicebear.com/api/pixel-art/${coder.replace("#", "")}.svg`} />
-                  <span>{coder}</span>
-            </div>)}
+            <div className="col-8 d-flex justify-content-center flex-wrap">
+              {coders.map((coder, key) => <div className="d-flex flex-column align-items-center" key={key}>
+                <img width="150" src={`https://avatars.dicebear.com/api/pixel-art/${coder}.svg`} />
+                <span>{coder}</span>
+              </div>)}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>);
+    </div>);
 };
 
 export default App;
